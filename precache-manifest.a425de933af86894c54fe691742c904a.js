4d4a7fff5a68afd8df0610a13c7dbde31c3a6a40
awttt <350944364@qq.com>
2020-05-24 16:42:43 +0800

    deploy

---
+++ precache-manifest.a425de933af86894c54fe691742c904a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd919e4de99b5a56a7a",
+    "url": "/money-website/css/app.1d073f54.css"
+  },
+  {
+    "revision": "8e774578c8bf9a4703c8ab515942653d",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "dbd919e4de99b5a56a7a",
+    "url": "/money-website/js/app.45cfcf5b.js"
+  },
+  {
+    "revision": "ce046edf5de1e92b17aa",
+    "url": "/money-website/js/chunk-vendors.8ca6ca1a.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/money-website/robots.txt"
+  }
+]);
